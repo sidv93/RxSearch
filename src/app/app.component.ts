864710c25c3c11188908bfc013894bdcfab0e8ba
Siddharth <siddhuv93@gmail.com>
2019-06-24 20:22:26 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public searchForm: any;
+  public results$: Observable<any>;
+
+  public constructor(private fb: FormBuilder, private http: Http) {
+    this.searchForm= this.fb.group({
+      'search': ['', Validators.required]
+    });
+    this.results$= this.searchForm.controls.search.valueChanges
+      .debounceTime(500)
+      .switchMap(query => {
+        console.log("value="+ query);
+        return this.http.get('http://localhost:3000/api/getAllUsers');
+      })
+      .map(res => res.json().data);
+  }
 }
